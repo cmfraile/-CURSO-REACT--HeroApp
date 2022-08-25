d69edefe76fabc23c6041081bd6484ccad9ae839
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-25 12:10:03 +0200

    189

--- src/heroapp.tsx
+++
@@ -1,5 +0,0 @@
-export const Heroapp = () => {
-    return(
-        <h1>Heroes App</h1>
-    )
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Heroapp } from './heroapp';
 import { BrowserRouter , Routes , Route, Navigate } from 'react-router-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './main.sass';
+
 import { Marvel } from './heroes/pages/Marvel';
 import { Dc } from './heroes/pages/Dc';
 import { Login } from './auth/pages/login';
+import { Navbar } from '../src/ui/components';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter><Routes>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
       <Route path="/marvel" element={<Marvel />}></Route>
       <Route path="/dc" element={<Dc />}></Route>
       <Route path='/' element={<Login />}></Route>
