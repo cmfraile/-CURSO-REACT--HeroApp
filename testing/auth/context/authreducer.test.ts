258248a5c5993c2c969e6ff0f53417f678f3bf02
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-09-06 15:42:05 +0200

    achuche

---
+++ testing/auth/context/authreducer.test.ts
@@ -0,0 +1,25 @@
+/*
+Pruebas de authreducer:
+    -   Debe de retornar el estado por defecto.
+    -   Debe de llamar el login, autenticar, y establecer el user.
+    -   Debe de hacer logout, borrar el nombre y poner el logged en false ( nombre en undefined tambien messirve ).
+*/
+
+const main = () => {
+    
+    test('Debe de retornar el estado por defecto',() => {
+
+    });
+
+    test('Debe de llamar el login, autenticar, y establecer el user.',() => {
+
+    });
+
+    test('Debe de hacer logout, borrar el nombre y poner el logged en false',() => {
+
+    })
+
+
+}
+
+describe('Pruebas de authreducer',main)
--- testing/main.test.ts
+++
@@ -1,5 +0,0 @@
-const main = () => {
-    test('prueba de prueba',() => {});
-}
-
-describe('Prueba de prueba, de prueba',main);
