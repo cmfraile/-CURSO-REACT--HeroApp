e889b8c36d8d806097c4e94c721fa1a72e3848b1
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-09-07 12:11:28 +0200

    vamos a hacer la tarea 225

---
+++ testing/ui/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+
+
+//1 - El Navbar debe de mostrar el nombre de usuario
+//2 - y el logout | debe de hacer sus dos funciones:
+
+const main = () => {
+    test('',() => {});
+    test('',() => {});
+}
+
+describe('',main);
